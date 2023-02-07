a0f1b149fc2a1ada17e37392322417de83da23bf
MSblasttor <msblast@bk.ru>
2023-02-07 22:24:07 +0300

    Start lesson Skillbox. Made web messanger

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ from datetime import datetime
 from flask import Flask, render_template, request
 import json
 
-db_name = 'db_json'
+db_name = 'db.json'
+
 
 def load_info(db_name):
     with open(db_name, 'r') as db:
         db_info = json.load(db)
-    return db_info
+    return db_info['messages']
+
+
+def save_info(db_info, db_name):
+    data = {'messages': db_info}
+    with open(db_name, 'w') as json_file:
+        json.dump(data, json_file)
+
 
 app = Flask(__name__)
 
+all_messages = load_info(db_name)
 @app.route('/index')
 def index_page():
     return "Hello world!"
@@ -19,4 +28,23 @@ def index_page():
 def display_chat():
     return render_template('form.html')
 
-app.run(host='0.0.0.0', port=80)
+@app.route('/get_messages')
+def get_message():
+    return {'messages': all_messages}
+
+@app.route('/send_message')
+def send_message():
+    sender = request.args['name']
+    text = request.args['text']
+    time = datetime.now().strftime('%H:%M')
+    text_info = {
+        'text': text,
+        'sender': sender,
+        'time': time
+    }
+    all_messages.append(text_info)
+    save_info(all_messages, db_name)
+
+
+
+app.run(host='0.0.0.0', port=5050)
